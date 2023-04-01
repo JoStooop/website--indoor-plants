5e1dc0f61ce81b18e09b9b9bcf96f268eda7adbf
Ivan Timoshenko <ivantm.dev@gmail.com>
2023-04-01 02:17:34 +0200

    [main] indoor-plants

--- client/src/components/main/shop/shop-top-block/ShopTopBlock.jsx
+++ client/src/components/main/shop/shop-top-block/ShopTopBlock.jsx
@@ -1,13 +1,10 @@
 import React from "react";
 import fiddle from "@/assets/fiddle.jpg";
 
-const BLOCK_HEIGHT = "600px";
-const BACKGROUND_IMAGE = `linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.3)), url(${fiddle})`;
-
 const ShopTopBlock = () => {
   return (
-    <div className={`bg-center md:bg-cover bg-fixed bg-no-repeat h-[${BLOCK_HEIGHT}] md:w-full`}
-      style={{backgroundImage: BACKGROUND_IMAGE}}>
+    <div className={"bg-center md:bg-cover bg-fixed bg-no-repeat h-[600px] md:w-full"}
+      style={{backgroundImage: `linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.3)), url(${fiddle})`}}>
       <h1 className="flex text-3xl md:text-5xl h-full m-auto md:ml-auto md:mt-auto w-4/5 md:w-1/2
            text-white font-display justify-center items-center">
         A Plant for Every Space
